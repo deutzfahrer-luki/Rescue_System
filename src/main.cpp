59f258becf2216b16a7e632768e32e8557a34336
Lukas <traktor.koeppl@gmail.com>
2025-01-06 11:41:06 +0100

    update the main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,50 @@
 
 #include <RescueSystem.h>
 
-
-// RescueSystem RSystem();
-
 RescueSystem rescueSystem;
+
+uint8_t dir;
+uint8_t oldDir = dir;
+  uint8_t result[2];
+
+void splitNumber(uint8_t number, uint8_t* result) {
+    result[0] = number / 10;
+    result[1] = number % 10;
+}
+
+void updatePWMFromSerial()
+{
+  if (Serial.available() > 0)
+  {
+    float newDir = Serial.parseInt();
+    if (newDir >= 0 && newDir <= 99)
+    {
+      dir = newDir;
+      splitNumber(dir, result);
+      Serial.print("Result: [");
+      Serial.print(result[0]);
+      Serial.print(", ");
+      Serial.print(result[1]);
+      Serial.println("]");
+    }
+
+    else
+      Serial.println("Ungueltiger Wert");
+    while (Serial.available() > 0)
+      Serial.read();
+  }
+}
 
 
 void setup() {
-
+  rescueSystem.begin();
 }
 
 void loop() {
   rescueSystem.update();
-  // RSystem.
+  if (dir!=oldDir)
+  {
+    oldDir = dir;
+    rescueSystem.setServoManually(result[0], result[1]);
+  }
 }
-
